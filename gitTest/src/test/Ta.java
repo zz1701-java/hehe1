bcfa3306fc2b439e1e4f9f627d8e53e40c7fac57
Administrator <Administrator@A-201703011254>
2017-06-09 12:09:32 +0800

    删除

--- gitTest/src/test/Ta.java
+++ gitTest/src/test/Ta.java
@@ -4,8 +4,7 @@ public class Ta {
 	public int sum(int x,int y){return x+y;};
 	public static void main(String[] args) {
 		Ta ta=new Ta();
-		ta.sum(5, 70);
-		System.out.println("a");
+		
 		System.out.println("c");
 	}
 	
